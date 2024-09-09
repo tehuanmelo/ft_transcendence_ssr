1a788e251c1ee8182ddb045d129c46f226cad63f
tehuanmelo <tehuanmelo@gmail.com>
2024-09-09 22:21:11 +0400

    spa working login logout

--- django/static/js/script.js
+++ django/static/js/script.js
@@ -1,46 +1,92 @@
+document.addEventListener('DOMContentLoaded', function () {
 
+    document.body.addEventListener('click', function(event) {
+        event.preventDefault();
+    
+        if (event.target.matches('.login')) {
+            getLogin();
+        } else if (event.target.matches('.login-button')) {
+            postLogin();
+        } else if (event.target.matches('.logout-link')) {
+            getLogout();
+        }
+    });
 
-document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('submit', function (event) {
+        event.preventDefault();
+        
+        if (event.target.matches('#login-form')) {
+            alert('submit detectado');
 
-    const navLinks = document.querySelectorAll('.nav-link');
-    const mainContent = document.querySelector('#main-content');
-
-    navLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            const url = this.getAttribute('href');
-
-            history.pushState(null, '', url);
-
-            fetch(url)
-            .then(response => {
-                if (!response.ok) throw new Error('Invalid response');
-                return response.text();
-            })
-            .then(html => {
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(html, 'text/html');
-                mainContent.innerHTML = doc.querySelector("#main-content").innerHTML;
-            })
-            .catch(err => console.error('Error fecthing the page', err.message));
-        })
-    })
-
-    window.addEventListener('popstate', function () {
-        const url = location.pathname;
-        fetch(url)
-            .then(response => {
-                if (!response.ok) throw new Error('Invalid response');
-                return response.text();
-            })
-            .then(html => {
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(html, 'text/html');
-                mainContent.innerHTML = doc.querySelector("#main-content").innerHTML;
-                
-            })
-            .catch(err => console.error('Error fecthing the page', err.message));
+        }
     });
 
 
-})
+});
+
+function getLogin() {
+    fetch('users/login')
+    .then(response => {
+        if (!response.ok) throw new Error('Invalid response');
+        return response.text();
+    })
+    .then(html => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        const newContent = doc.querySelector('#main-content').innerHTML;
+        document.querySelector('#main-content').innerHTML = newContent;
+    })
+    .catch(err => console.error('Error fetching the page', err.message));
+}
+
+
+
+function getLogout() {
+    fetch('users/logout')
+    .then(response => {
+        if (!response.ok) throw new Error('Invalid response');
+        return response.text();
+    })
+    .then(html => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        const newContent = doc.querySelector('#main-content').innerHTML;
+        document.querySelector('#main-content').innerHTML = newContent;
+    })
+    .catch(err => console.error('Error fetching the page', err.message));
+}
+
+
+
+function postLogin() {
+    console.log('inside post login');
+    const form = document.querySelector('#login-form'); 
+
+    const formData = new FormData(form);
+
+
+    fetch('users/login/', {
+        method: 'POST',
+        headers: {
+            'X-CSRFToken': formData.get('csrfmiddlewaretoken') // Inclui o token CSRF
+        },
+        body: formData 
+    })
+    .then(response => {
+        if (!response.ok) throw new Error('Invalid response');
+        console.log(response);
+        return response.text();
+    })
+    .then(html => {
+        console.log(html);
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        const newContent = doc.querySelector('#main-content').innerHTML;
+        document.querySelector('#main-content').innerHTML = newContent;
+    })
+    .catch(error => {
+        console.error('Login error:', error);
+    });
+}
+
+
--- django/users/views.py
+++ django/users/views.py
@@ -1,18 +1,22 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login
 from django.shortcuts import render
-from django.http import JsonResponse
-from django.template.loader import render_to_string
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
-        
+        print(request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return render(request, 'pages/home.html')
+            return render(request, "index.html")
     else:
         form = AuthenticationForm()
 
     return render(request, 'users/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('index') 
